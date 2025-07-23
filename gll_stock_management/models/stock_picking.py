ad750494ef9c7af78f3cfb2dab32f2b8a4b7c320
jackjack82 <giacomo.grasso.82@gmail.com>
2025-07-23 10:00:57 +0200

    finalizations

--- gll_stock_management/models/stock_picking.py
+++ gll_stock_management/models/stock_picking.py
@@ -9,8 +9,24 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     delivery_partner_id = fields.Many2one("res.partner", string="Delivery Partner")
+    items_count = fields.Integer(
+        compute="compute_items_count_volume",
+        store=True,
+        string="Number of items",
+    )
+    items_volume = fields.Float(
+        compute="compute_items_count_volume", store=True, string="Total volume (m2)"
+    )
 
     @api.onchange("delivery_partner_id")
     def _onchange_delivery_partner(self):
         if self.delivery_partner_id:
             self.location_dest_id = self.delivery_partner_id.internal_location_id
+
+    @api.depends("move_line_ids.quantity")
+    def compute_items_count_volume(self):
+        for picking in self:
+            picking.items_volume = sum(
+                sml.quantity * sml.product_id.volume for sml in self.move_line_ids
+            )
+            picking.items_count = sum(sml.quantity for sml in self.move_line_ids)
