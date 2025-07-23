3f4a1f853904a2f648656b26f90a3a96bcf94291
Andrea D'Ippolito <dippolito@webmonks.it>
2025-07-23 10:54:07 +0200

    init

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# © 2025 webmonks
+{
+    "name": "GLL Stock management",
+    "category": "Inventory",
+    "summary": "This module handle GLL stock management.",
+    "license": "OPL-1",
+    "description": "GLL Stock Workflow",
+    "version": "18.0.1.1.1",
+    "author": "Webmonks SRL",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": [
+        "stock_delivery",
+        "stock",
+    ],
+    "external_dependencies": {},
+    "assets": {
+        "web.assets_backend": [],
+    },
+    "data": [
+        # View files
+        "views/stock_picking.xml",
+        "views/res_partner.xml",
+    ],
+    # Technical
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
