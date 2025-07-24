3eaee7c16c6bbcc97cbf7b02fcdd1dfcf8987cfe
jackjack82 <giacomo.grasso.82@gmail.com>
2025-07-24 09:14:12 +0200

    fix packages computations

--- gll_stock_management/models/stock_picking.py
+++ gll_stock_management/models/stock_picking.py
@@ -1,21 +1,22 @@
-import logging
-
-from odoo import _, api, fields, models
-
-_logger = logging.getLogger(__name__)
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    delivery_partner_id = fields.Many2one("res.partner", string="Delivery Partner")
+    delivery_partner_id = fields.Many2one(
+        "res.partner",
+        string="Delivery Partner",
+    )
     items_count = fields.Integer(
         compute="compute_items_count_volume",
         store=True,
         string="Number of items",
     )
     items_volume = fields.Float(
-        compute="compute_items_count_volume", store=True, string="Total volume (m2)"
+        compute="compute_items_count_volume",
+        store=True,
+        string="Total volume (m2)",
     )
 
     @api.onchange("delivery_partner_id")
@@ -29,4 +30,4 @@ class StockPicking(models.Model):
             picking.items_volume = sum(
                 sml.quantity * sml.product_id.volume for sml in self.move_line_ids
             )
-            picking.items_count = sum(sml.quantity for sml in self.move_line_ids)
+            picking.items_count = len(self.move_ids.move_line_ids.result_package_id)
