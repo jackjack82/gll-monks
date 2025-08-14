e8b43d90a23a12e585ca6abc726f045898f02e53
jackjack82 <giacomo.grasso.82@gmail.com>
2025-08-14 18:06:20 +0200

    new fields on picking and gll.trip

---
+++ gll_stock_management/models/res_config_settings.py
@@ -0,0 +1,21 @@
+# © 2025 webmonks
+
+from odoo import api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    service_for_box_id = fields.Many2one(
+        "product.product",
+        string="Servizio per scatola",
+        domain=[("type", "=", "service")],
+        config_parameter="gll_stock_management.service_for_box_id",
+    )
+    
+    service_for_single_id = fields.Many2one(
+        "product.product",
+        string="Servizio per prodotto singolo",
+        domain=[("type", "=", "service")],
+        config_parameter="gll_stock_management.service_for_single_id",
+    )
