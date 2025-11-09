a48521b0b08a442183004bd0945678c8e3ece107
jackjack82 <giacomo.grasso.82@gmail.com>
2025-11-09 11:55:14 +0100

    changing values to vals

--- gll_stock_management/models/uom.py
+++ gll_stock_management/models/uom.py
@@ -6,4 +6,6 @@ from odoo import fields, models
 class UoM(models.Model):
     _inherit = "uom.uom"
 
-    import_code = fields.Char(string="Import Code", help="Code used for importing UoM from external files")
+    import_code = fields.Char(
+        string="Import Code", help="Code used for importing UoM from external files"
+    )
