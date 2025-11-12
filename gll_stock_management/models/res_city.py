78d9414d5a177866ba1e3f88046ab07d926f2ef6
jackjack82 <giacomo.grasso.82@gmail.com>
2025-11-12 14:52:45 +0100

    city inconvenient import finalized

--- gll_stock_management/models/res_city.py
+++ gll_stock_management/models/res_city.py
@@ -4,14 +4,13 @@ from odoo import fields, models
 
 
 class City(models.Model):
-    _inherit = 'res.city'
+    _inherit = "res.city"
 
     inconvenient_place_ids = fields.Many2many(
-        'delivery.carrier',
-        'res_city_inconvenient_place_rel',
-        'city_id',
-        'carrier_id',
-        string='Inconvenient Place',
-        help='Delivery carriers that consider this city as an inconvenient place',
+        "delivery.carrier",
+        "res_city_inconvenient_place_rel",
+        "city_id",
+        "carrier_id",
+        string="Inconvenient Place",
+        help="Delivery carriers that consider this city as an inconvenient place",
     )
-    
